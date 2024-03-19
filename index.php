aa5e7a06716f5254a95b82b4546c2b5f787d551f
dimeh1 <158572119+dimeh1@users.noreply.github.com>
2024-03-19 09:15:32 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>4RouesMotrices</title>
+    <link rel="icon" href="Image/Logo.jpg" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="stylesheet.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+
+    </header>
+    <div id="menutaille">
+        <div id="menu1">
+            <h2>4 R0ues M0trices</h2>
+            <a href="index.html"><h3>Accueil</h3></a>
+        </div>
+        <div id="menu2">
+            <h2>Nos services</h2>
+            <ul>
+                <a href="hypercar.html"><li>Hypercar</li></a>
+                <a href="sport.html"><li>Sport</li></a>
+                <a href="concessionaire.html"><li>Concessionaire</li></a>
+                <a href="contact.html"><li>Contact</li></a>
+            </ul>
+        </div>
+    </div>
+    <div id="main">
+        <div class="container" id="container">
+            <div class="form-container créer-compte">
+                <form action="#">
+                    <h1>Créer un compte</h1>
+                    <span><i>Utiliser compte gmail</i></span>
+                        <input type="text" placeholder="Nom">
+                        <input type="text" placeholder="Prénom">
+                        <input type="gmail" placeholder="Email">
+                        <input type="password" placeholder="Mot de passe">
+                        <button>Créer compte</button>
+                        <p>Déjà un compte ? <a href="login.html">Connectez-vous ici</a>.</p>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <footer> 
+    </footer>
+</body>
+</html>
+
